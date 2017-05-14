0f739a445d26f732cbdb7df3a1bc99f9c2faa3d7
shenshuai89 <shenshuai89@qq.com>
2017-05-14 22:54:10 +0800

    第一次的后台管理页面

--- blog/routers/admin.js
+++ blog/routers/admin.js
@@ -5,8 +5,18 @@
 var express = require('express')
 var router = express.Router()
 
-/*router.get('/user', function (req, res, next) {
-    res.send('User')
-})*/
+router.use(function (req, res, next) {
+    if(!req.userInfo.isAdmin){
+        res.send("权限不够，只有管理员才可以进入后台管理")
+        return
+    }
+    next()
+})
+
+router.get('/', function (req, res, next) {
+    res.render('admin/index',{
+        userInfo: req.userInfo
+    })
+})
 
 module.exports = router
